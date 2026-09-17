50802a3d60607d1200e915bce50c01e17ac7df92
Caliboy <164075921+Caliboy6@users.noreply.github.com>
2026-09-17 19:55:20 +0800

    Update module references for opennext-portal.js

--- opennext-portal.js
+++ opennext-portal.js
@@ -3,9 +3,9 @@ import { demandTape } from './procurement-data.js?v=opennext-20260914-desk-1';
 import { renderCapacityPage, initializeCapacity, addDemoAllocation } from './opennext-capacity.js?v=opennext-20260915-marketplace-1';
 import { renderAccountPage, initializeAccount, closeAccountDialogs } from './opennext-account.js?v=opennext-20260915-marketplace-1';
 import { createWorkspaceStore } from './opennext-portal-state.js?v=opennext-20260914-desk-1';
-import { renderOpenDesk, initializeOpenDesk } from './opennext-desk.js?v=opennext-20260914-desk-1';
-import { createMarketplaceStore } from './opennext-marketplace-state.js?v=opennext-20260915-marketplace-2';
-import { initializeMarketplace, renderMarketplacePage, openMarketplaceComposer } from './opennext-marketplace.js?v=opennext-20260915-marketplace-2';
+import { renderOpenDesk, initializeOpenDesk } from './opennext-desk.js?v=opennext-20260917-market-perspective-1';
+import { createMarketplaceStore } from './opennext-marketplace-state.js?v=opennext-20260917-rental-terms-1';
+import { initializeMarketplace, renderMarketplacePage, openMarketplaceComposer } from './opennext-marketplace.js?v=opennext-20260917-rental-terms-1';
 
 const sections=[['opendesk','My OpenDesk'],['profile','Profile'],['tokens','My Tokens'],['my-gpus','My GPUs'],['supply','My Supplies'],['rfq','My RFQs'],['messages','Messages'],['billing','Billing'],['account','Account']];
 const personal=new Set(sections.map(([id])=>id));
